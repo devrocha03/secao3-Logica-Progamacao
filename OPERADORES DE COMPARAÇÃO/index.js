f064268b6b44aec19d658d70a41aac52fd111800
devrocha03 <devrocha03@gmail.com>
2025-11-08 17:47:34 -0300

    Operadores de comparacão

--- OPERADORES DE COMPARAÇÃO/index.js
+++ OPERADORES DE COMPARAÇÃO/index.js
@@ -7,8 +7,7 @@ Operadores de Comparação
     ===  igualdade estrita (valor e tipo)
     !== diferente estrito  (valor e tipo)
 */
-const num1 = 1
-0;
+const num1 = 10;
 const num2 = 10;
 const comp = num1 !== num2;
 console.log(comp);
