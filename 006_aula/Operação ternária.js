11b70207005aec1abab4897feca1d900d2f7a96e
devrocha03 <devrocha03@gmail.com>
2025-11-13 15:51:29 -0300

    Operação ternária

---
+++ 006_aula/Operação ternária.js
@@ -0,0 +1,16 @@
+// (condição) ? Valor para Verdadeiro : Valor para Falso
+
+const usuario = 2222;
+const nivelUsuario = usuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUsuario);
+
+
+
+/* 
+if (usuario >= 1000) {
+  console.log('Usuário VIP');
+} else {
+  console.log('Usuário Normal');
+}
+*/
+
