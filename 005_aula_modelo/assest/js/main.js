e3f98a8a5568bd3127a157f6b8ded7b38fb972a2
devrocha03 <devrocha03@gmail.com>
2025-11-13 15:18:56 -0300

    Calcular IMC (HTML, CSS, JS)

--- 005_aula_modelo/assest/js/main.js
+++ 005_aula_modelo/assest/js/main.js
@@ -1,4 +1,114 @@
-function calcularIMC() {
+const form = document.querySelector("#formulario"); // CAPTURA DO FORMULÁRIO DO FORM NO HTML
+
+form.addEventListener("submit", function (e) { 
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado('Peso Inválido', false);
+    return;
+  }
+  if (!altura) {
+    setResultado('Altura Inválida', false);
+    return;
+  }
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${nivelImc})`;
+
+  setResultado(msg, true);
+   
+});
+
+function getImc(peso, altura) {
+  const imc = peso / (altura * altura)
+  return imc.toFixed(2);
+}
+
+function getNivelImc(imc) {
+  const nivel = ['Abaixo do Peso', 'Peso Normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3'];
+
+  if (imc >= 39.9) {
+    return nivel[5];
+  }
+  if (imc >= 34.9) {
+    return nivel[4];
+  }
+  if (imc >= 29.9) {
+    return nivel[3];
+  }
+  if (imc >= 24.9) {
+    return nivel[2];
+  }
+  if (imc >= 18.5) {
+    return nivel[1];
+  }
+  if (imc < 18) {
+    return nivel[0];
+  }
+}
+
+
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+
+  const p = criaP();
+
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
+    
+  }
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function calcularIMC() {
   const form = document.querySelector(".form");
 
   function recebeEventoForm(evento) {
@@ -24,7 +134,7 @@ function calcularIMC() {
     } else if (imc >= 35 && imc <= 39.9) {
       botao.innerHTML = `Seu IMC é de ${imc.toFixed(2)} (Obesidade Grau 2) `;
     } else if (imc >= 40) {
-      botao.innerHTML = `Seu IMC é de ${imc.toFixed(2)} (Obesidade Grau 3 ksdfsadf) `;
+      botao.innerHTML = `Seu IMC é de ${imc.toFixed(2)} (Obesidade Grau 3) `;
       return;
     }
   }
@@ -32,4 +142,4 @@ function calcularIMC() {
   form.addEventListener("submit", recebeEventoForm);
 }
 
-calcularIMC();
+calcularIMC(); */
