975aae143ca0d9b58ecd7be3f10404e157ffa012
devrocha03 <devrocha03@gmail.com>
2025-11-18 13:38:54 -0300

    var, let e const. Outros conceitos

---
+++ 010_aula/var, let e const.js
@@ -0,0 +1,28 @@
+const verdadeira = true
+
+// LET tem escopo de BLOCO {....}
+// VAR só tem escopo de função
+
+// let nome = "joao";
+// var nome2 = "joao";
+
+// if (verdadeira) {
+//   let nome = "alexandre"
+//   console.log(nome, nome2);
+
+//   if (verdadeira) {
+//     let nome = "rocha"
+//     console.log(nome, nome2);
+//   }
+// }
+
+function falaOi() {
+  
+  if (verdadeira) {
+    let nome = 'Rogério'
+    var sobrenome = "Mendes"
+  }
+  console.log(sobrenome);
+}
+
+falaOi();
