a3317a41896b0bd0a422446e6e8a660f7a7659fb
devrocha03 <devrocha03@gmail.com>
2025-11-19 17:20:25 -0300

    exercicio mudar bg com for (js)

--- 017_aula_exercicio/NodeList (Browser).js
+++ 017_aula_exercicio/NodeList (Browser).js
@@ -1,11 +1,28 @@
 const p = document.querySelector(".paragrafos");
 const paragrafos = document.querySelectorAll('p');
 
+const container = document.querySelector(".container")
+const h1 = document.querySelectorAll('h1');
+const h2 = document.querySelectorAll('h2');
+
 const estiloBody = getComputedStyle(document.body);
 const bgColorBody = estiloBody.backgroundColor;
+
 
 for (let p of paragrafos) {
   p.style.backgroundColor = bgColorBody
   p.style.color = '#FFF'
+  p.style.fontFamily = "georgia"
+  p.style.fontSize = '18px'
   
+}
+
+for (let container of h1) {
+  container.style.color = "#c50303ff"
+  container.style.fontFamily = "verdana"
+}
+
+for (let container of h2) {
+  container.style.color = "#3e2b85ff"
+  container.style.fontFamily = "arial"
 }
